13c4168c84efc608c89831bf265824b9ed8e33ab
TheuZCoder <matheusrdsilva1@gmail.com>
2023-09-12 15:40:50 -0300

    att

--- src/CalcTemp/TemperaturaBottom.java
+++ src/CalcTemp/TemperaturaBottom.java
@@ -1,5 +1,13 @@
 package CalcTemp;
 
 public class TemperaturaBottom {
+    public TemperaturaBottom(){
+        super();
+        this.setLayout(new BorderLayout());
+
+        this.add(new JTextArea(5, 50));
+
+        this.setBorder(new EtchedBorder());
+    }
     
 }
