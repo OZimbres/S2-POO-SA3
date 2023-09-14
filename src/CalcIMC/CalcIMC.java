62e9464571f94e1d3405357835b8ce405da6feac
TheuZCoder <matheusrdsilva1@gmail.com>
2023-09-14 15:50:59 -0300

    att imc

--- src/CalcIMC/CalcIMC.java
+++ src/CalcIMC/CalcIMC.java
@@ -1,63 +1,80 @@
 package CalcIMC;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalcIMC extends JPanel {
-   private JTextField pesoTextField, alturaTextField, resultadoTextField;
+    private JTextField pesoTextField, alturaTextField, resultadoTextField;
     private JButton calcularButton;
 
     public CalcIMC() {
-
         JLabel pesoLabel = new JLabel("Peso (kg):");
-        JLabel alturaLabel = new JLabel("Altura (m):");
+        JLabel alturaLabel = new JLabel("Altura (cm):");
         JLabel resultadoLabel = new JLabel("IMC:");
 
         pesoTextField = new JTextField();
         alturaTextField = new JTextField();
         resultadoTextField = new JTextField();
         resultadoTextField.setEditable(false);
-
         calcularButton = new JButton("Calcular");
+
+        
+        // Criação dos painéis
+        JPanel painelPrincipal = new JPanel(new BorderLayout()); // Border Layout
+        // this.add(painelPrincipal);
+        JPanel painelBotoes = new JPanel();
+        // this.add(painelBotoes);
+        JPanel painelInputs = new JPanel(new GridLayout(4, 4));
+        // this.add(painelInputs);
+        JPanel painelResultado = new JPanel();
+        // this.add(painelResultado);
+
+        // adicionando os componentes aos painéis
+        painelInputs.add(alturaLabel);
+        // painelInputs.add(new JLabel(""));
+        painelInputs.add(alturaTextField);
+
+        painelInputs.add(pesoLabel);
+        // painelInputs.add(new JLabel(""));
+        painelInputs.add(pesoTextField);
+
+        painelBotoes.add(calcularButton);
+
+        painelResultado.add(resultadoLabel);
+        painelResultado.add(resultadoTextField);
+
+        // adicionando os painéis ao painel principal
+        painelPrincipal.add(painelInputs, BorderLayout.NORTH);
+        painelPrincipal.add(painelBotoes, BorderLayout.CENTER);
+        painelPrincipal.add(painelResultado, BorderLayout.SOUTH);
+
+        this.add(painelPrincipal);
         calcularButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                calcularIMC();
+                calcIMC();
             }
         });
-
-        this.add(pesoLabel);
-        this.add(pesoTextField);
-        this.add(alturaLabel);
-        this.add(alturaTextField);
-        this.add(resultadoLabel);
-        this.add(resultadoTextField);
-        this.add(calcularButton);
     }
 
-    private void calcularIMC() {
+    private void calcIMC() {
         try {
             double peso = Double.parseDouble(pesoTextField.getText());
             double altura = Double.parseDouble(alturaTextField.getText());
 
-            double imc = peso / (altura * altura);
+            double imc = peso / ((altura / 100) * (altura / 100));
 
             resultadoTextField.setText(String.format("%.2f", imc));
         } catch (NumberFormatException e) {
             resultadoTextField.setText("Erro");
         }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new CalcIMC();
-        });
-    }
+    } 
 }
