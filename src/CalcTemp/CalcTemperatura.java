d8111db1c859292b934096fd280dbb8e0fc06b07
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-09-15 03:01:39 -0300

    Deleting unecessary imports

--- src/CalcTemp/CalcTemperatura.java
+++ src/CalcTemp/CalcTemperatura.java
@@ -4,8 +4,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
